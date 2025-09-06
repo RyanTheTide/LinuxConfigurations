1bdf9ec8770c2804f624c7552968a91018fc17b5
Ryan <81541646+RyanTheTide@users.noreply.github.com>
2025-09-06 21:46:19 +1000

    Fix formatting in welcome message for clarity

--- scripts/newinstall/lib/input/welcome.sh
+++ scripts/newinstall/lib/input/welcome.sh
@@ -3,19 +3,19 @@
 input_welcome() {
   clear
   cat <<EOF
-  Welcome to ArchInstaller by RyanTheTide!
+Welcome to ArchInstaller by RyanTheTide!
 
-  This script will install a Arch Linux system
-  with a guided, interactive process. Offering
-  sensible defaults while allowing customization.
+This script will install a Arch Linux system
+with a guided, interactive process. Offering
+sensible defaults while allowing customization.
 
-  The base system will be installed with the following:
-  - btrfs filesystem (@,@home,@snapshots,@cache,@log)
-  - rEFInd bootmanager (with custom rounddark theme)
-  - additional packages (developement, networking, etc)
-  - microcode (if detected intel or amd)
-  - pacman mirrors (organised by speed, set by country)
+The base system will be installed with the following:
+- btrfs filesystem (@,@home,@snapshots,@cache,@log)
+- rEFInd bootmanager (with custom rounddark theme)
+- additional packages (developement, networking, etc)
+- microcode (if detected intel or amd)
+- pacman mirrors (organised by speed, set by country)
 
-  Thanks for using my script & follow the folowing prompts!
+Thanks for using my script & follow the folowing prompts!
 EOF
 }
