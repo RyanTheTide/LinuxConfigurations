66e0b3e4eca6290e6262bf6fcdbb0b903e111e13
Ryan <81541646+RyanTheTide@users.noreply.github.com>
2025-09-06 21:32:29 +1000

    Enhance disk selection prompt in disk.sh
    Added a prompt to guide users in selecting a target disk for installation.

--- scripts/newinstall/lib/input/disk.sh
+++ scripts/newinstall/lib/input/disk.sh
@@ -4,6 +4,7 @@
 
 input_disk() {
     local __var
+    printf "Let's get started by selecting a target disk for installation!"
     printf '%.0s-' {1..54}; echo
     lsblk -dn -o NAME,SIZE,MODEL | grep -v '^loop' | awk '{print "/dev/" $1, "-", $2, "-", substr($0, index($0,$3))}'
     printf '%.0s-' {1..54}; echo
